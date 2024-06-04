489960f0b27bb0b6cdf7582e5fa3f74f26b0b450
JerryY999 <jerry.yuan@mantle.xyz>
2024-06-04 22:29:16 +0800

    fix: update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ const webapp = window.Telegram.WebApp
 if(webapp){
   webapp.ready()
   console.log("Telegram--start")
-  console.log(webapp.initDataUnsafe)
+  console.log(webapp.initDataUnsafe.username)
   console.log("----end----")
 }
 
