d53e40a64c019bf9e2239a965e304e8658dcf783
JerryY999 <jerry.yuan@mantle.xyz>
2024-06-04 22:13:24 +0800

    fix: udpate

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ if(webapp){
   console.log("Telegram--start")
   console.log(webapp.initDataUnsafe.user?.id)
   console.log(webapp.initDataUnsafe.user)
-  console.log(window.Telegram)
+  console.log(window.Telegram.WebAppUser)
   console.log("----end----")
 }
 
