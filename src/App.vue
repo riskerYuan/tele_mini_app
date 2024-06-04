f25dbb00adcd4ba29b3893e1a3a5583fe704afa4
JerryY999 <jerry.yuan@mantle.xyz>
2024-06-04 21:38:50 +0800

    fix :

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+const webapp = window.Telegram.WebApp
+if(webapp){
+  webapp.ready()
+  console.log("Telegram--start")
+  console.log(webapp)
+}
+
+const aaa= "aaa"
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    {{aaa}}
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
